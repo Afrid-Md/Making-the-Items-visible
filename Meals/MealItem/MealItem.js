7e5b37abf175f585ba07cfba7e512defa7b16f6f
afrid.md <122868488+Afrid-Md@users.noreply.github.com>
2023-05-01 14:03:38 +0530

    available meals list added

---
+++ Meals/MealItem/MealItem.js
@@ -0,0 +1,15 @@
+import classes from './MealItem.module.css'
+export default function MealItem(props){
+    return(
+        <li className={classes.meal}>
+            <div>
+                <h3>{props.name}</h3>
+                <div className={classes.description}>{props.description}</div>
+                <div className={classes.price}>Rs.{props.price}</div>
+            </div>
+            <div>
+
+            </div>
+        </li>
+    )
+}
---
+++ Meals/MealsSummary.js
@@ -0,0 +1,15 @@
+import classes from './MealsSummary.module.css'
+
+export default function MealsSummary(){
+    return(
+        <section className={classes.summary}>
+            <h2>Delicious meals will be delivered to you!</h2>
+            <p>
+                Choose your meal which are available in our restaurent, which are really delicious.
+            </p>
+            <p>
+                All meals are prepared with high quality ingredeints, and it will gives you a wonderful experience.
+            </p>
+        </section>
+    )
+}
